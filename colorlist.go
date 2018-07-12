a3cd2c4654b9914779bcd9a7a38b1401d9aae8a6
Aleksander Kiryk <alkiryk@wp.pl>
2018-07-12 22:10:13 +0200

    cleanup

--- colorlist.go
+++ colorlist.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"html/template"
 	"image/color"
 	"image/png"
 	"io"
@@ -10,102 +12,90 @@ import (
 	"sort"
 )
 
-type ColorFreq struct {
+var (
+	input   = flag.String("f", "", "input file (default stdin)")
+	reverse = flag.Bool("r", false, "reverse sorting")
+)
+
+var tmpl = template.Must(template.New("output").Parse(`
+<!DOCTYPE html>
+<html>
+	<head><style>td {min-width: 30pt; font-family: monospace;}</style></head>
+	<body>
+		<table>
+		{{range .}}
+			<tr><td style="background-color: {{.RGB}};width: 10pt;">{{.F}}</td></tr>
+		{{end}}
+		</table>
+	</body>
+</html>
+`))
+
+type byFreq []ColorStats
+type ColorStats struct {
 	C color.Color
 	F int
 }
 
-type ByColorFreq []ColorFreq
+func (cfs byFreq) Len() int           { return len(cfs) }
+func (cfs byFreq) Less(i, j int) bool { return cfs[i].F < cfs[j].F }
+func (cfs byFreq) Swap(i, j int)      { cfs[i], cfs[j] = cfs[j], cfs[i] }
 
-func (cfs ByColorFreq) Len() int {
-	return len(cfs)
+func (c *ColorStats) RGB() string {
+	r, g, b, _ := c.C.RGBA()
+	return fmt.Sprintf("#%02x%02x%02x", r/0x100, g/0x100, b/0x100)
 }
 
-func (cfs ByColorFreq) Swap(i, j int) {
-	cfs[i], cfs[j] = cfs[j], cfs[i]
+func PrintStats(w io.Writer, stats []ColorStats) {
+	tmpl.Execute(w, stats)
 }
 
-func (cfs ByColorFreq) Less(i, j int) bool {
-	/*ir, ig, ib, _ := cfs[i].C.RGBA()
-	jr, jg, jb, _ := cfs[j].C.RGBA()*/
-	if cfs[i].F > cfs[j].F {
-		return true
-	} /*else if cfs[i].F == cfs[j].F && ir > jr {
-		return true
-	} else if ir == jr && ig > jg {
-		return true
-	} else if ig == jg && ib > jb {
-		return true
-	}*/
-	return false
-}
-
-func ReadStats(r io.Reader) (map[color.Color]int, error) {
-	stats := make(map[color.Color]int)
+func ReadStats(r io.Reader) ([]ColorStats, error) {
 	img, err := png.Decode(r)
-
 	if err != nil {
 		return stats, err
 	}
 
+	counts := make(map[color.Color]int)
 	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
 		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
-			stats[img.At(x, y)]++
+			counts[img.At(x, y)]++
 		}
+	}
+
+	stats := []ColorStats{}
+	for c, f := range counts {
+		stats = append(stats, ColorStats{c, f})
 	}
 	return stats, nil
 }
 
-func GetTopN(stats map[color.Color]int, n int) []ColorFreq {
-	top := []ColorFreq{}
-
-	for c, f := range stats {
-		top = append(top, ColorFreq{c, f})
-	}
-	sort.Sort(ByColorFreq(top))
-	return top
-}
-
-func PrintStats(w io.Writer, cf []ColorFreq) {
-	fmt.Fprintf(w, "<!DOCTYPE HTML>\n<html>"+
-		"<head><style>td {min-width: 30pt;font-family: monospace;}</style></head>"+
-		"<body><table><tr>\n")
-	for i := range cf {
-		if (i+1)%10 == 0 {
-			fmt.Fprintf(w, "</tr><tr>\n")
-		}
-		r, g, b, _ := cf[i].C.RGBA()
-		r /= 0x100
-		g /= 0x100
-		b /= 0x100
-		fmt.Fprintf(w,
-			"\t<td style=\"background-color: rgb(%d,%d,%d);width: 10pt;\">%d</td>\n",
-			r, g, b, cf[i].F)
-	}
-	fmt.Fprintf(w, "</tr></table></body></html>\n")
-}
-
 func main() {
 	var err error
-	var r io.Reader
-	var w io.Writer
 
-	if len(os.Args) == 2 {
-		if r, err = os.Open(os.Args[1]); err != nil {
+	flag.Parse()
+
+	r, w := os.Stdin, os.Stdout
+	if *input != "" {
+		if r, err = os.Open(*input); err != nil {
 			log.Fatal(err)
-			os.Exit(1)
 		}
-		if w, err = os.Create(os.Args[1] + ".html"); err != nil {
+		defer r.Close()
+		if w, err = os.Create(*input+".html"); err != nil {
 			log.Fatal(err)
-			os.Exit(1)
 		}
-	} else {
-		r, w = os.Stdin, os.Stdout
+		defer w.Close()
 	}
+
 	stats, err := ReadStats(r)
 	if err != nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
-	PrintStats(w, GetTopN(stats, len(stats)))
+
+	if *reverse {
+		sort.Sort(sort.Reverse(byFreq(stats)))
+	} else {
+		sort.Sort(byFreq(stats))
+	}
+	PrintStats(w, stats)
 }
